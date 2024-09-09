40e8ff61a9ad9db64221f36318a7fc73ca3e39a2
Tomas Sarapura <tomisarapura@gmail.com>
2024-09-09 15:24:55 -0300

    Suma de botones

--- app/(tabs)/counter.tsx
+++ app/(tabs)/counter.tsx
@@ -1,19 +1,55 @@
 import { View, StyleSheet, Text, Pressable } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'expo-router';
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const sumar = () => {
+    setCount(count + 1);
+  };
+  const restar = () => {
+    if (count != 0) setCount(count - 1);
+  };
   return (
 
     
     <View style={styles.container}>
       <Text style={styles.title}>Counter</Text>
+        
+      <Text style= {{margin:5}}>
+        Cantidad = {count}
+      </Text>
+    
+      <Pressable
+          style={{
+            backgroundColor: "blue",
+            padding: 10,
+            borderRadius: 5,
+            margin: 5,
+          }}
+          onPress={sumar}
+        >
+          <Text style={{ color: "white" }}>Sumar</Text>
+      </Pressable>
+
+      <Pressable
+          style={{
+            backgroundColor: "red",
+            padding: 10,
+            borderRadius: 5,
+            margin: 5,
+          }}
+          onPress={restar}
+        >
+          <Text style={{ color: "white" }}>Restar</Text>
+      </Pressable>
+
       <Link href="/" asChild>
-        <Pressable >
+        <Pressable style={{borderRadius: 5}}>
           <Text style={styles.buttonText}>Home</Text>
         </Pressable>
       </Link>
-        
     </View>
   );
 }
@@ -34,6 +70,8 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
     fontWeight: "bold",
-    color: "blue",
+    padding: 10, 
+    backgroundColor: "black",
+    color: "white",
   },
 });
